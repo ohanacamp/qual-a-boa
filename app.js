fd1760e5128ab438b70eb31f0b08ff8a5d355985
loelias <loe_elias@msn.com>
2018-07-25 20:40:26 -0300

    hello world 2

--- app.js
+++ app.js
@@ -6,16 +6,8 @@ const PORT = process.env.PORT || 3000
 
 
 
-app.get('/', (req, res) => {
-    //res.send('Hello World!'))
-    const URL = 'https://api.meetup.com/find/upcoming_events'
-
-    request.get({ url: URL, qs: req.query, json: true}, (err, _, body) => {
-        err ? res.json(err): res.json(body)
-    })
-}) 
-
-
-app.use(express.static(path.join(__dirname, 'public')))
+app.get('/events', (req, res) => {
+    res.send('Hello World!')
+} 
 
 app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`))
