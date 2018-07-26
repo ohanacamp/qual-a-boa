b5290f1c5535f1cc8677b8adce6b64af3ca373f6
Pedro Ferreira <peferrera@outlook.com>
2018-07-26 17:35:26 -0300

    TASK07 - Fix EventList and Event style

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<div className="App-header__logo">
-						<img src="https://d30y9cdsu7xlg0.cloudfront.net/png/15343-200.png" alt="Qual a Boa ?"/>
+						<img className="logo"src="https://d30y9cdsu7xlg0.cloudfront.net/png/15343-200.png" alt="Qual a Boa Joinville?"/>
 					</div>
 					<div className="App-header__title">
 						<h1>Qual a boa ?</h1>
