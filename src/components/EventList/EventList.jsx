6ae31a131a59e3660a04213c7891e9f47998ead5
Pedro Ferreira <peferrera@outlook.com>
2018-07-26 14:51:59 -0300

    TASK04 - Done Displaying a list of all upcoming events in the next week in Joinville

--- src/components/EventList/EventList.jsx
+++ src/components/EventList/EventList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Event from "../Event/Event.jsx";
 
 export default class EventList extends React.Component {
     state = {
@@ -7,12 +8,13 @@ export default class EventList extends React.Component {
     }
 
     componentDidMount() {
-        axios.get(`http://localhost:5000/events`,{
-            params: {
-                lat: '-26.32',
-                lon: '-48.84'
-            }
-        })
+        axios.get('/events', {
+                params: {
+                    lat: '-26.31999969482422',
+                    lon: '-48.84000015258789',
+                    fields: 'featured_photo'
+                }
+            })
             .then(res => {
                 const listEvents = res.data;
                 this.setState({ events: listEvents.events });
@@ -20,15 +22,28 @@ export default class EventList extends React.Component {
     }
 
     render() {
+        
         return (
-            <div className="eventListCard">
-                <ul>
-                    {this.state.events.map((event, index) =>
-                        <li key={index}><a href={event.link}>{event.name}</a></li>
-                         )}
-                </ul>
-            </div>
+        <div className = "eventListCard">
+            <ul> 
+            {this.state.events.map((event, index) =>
+                <Event 
+                    key={index}
+                    link={event.link}
+                    name={event.name}
+                    date={event.local_date}
+                    time={event.local_time}
+                    //place={event.venue.name}
+                    //address={event.venue.address_1}
+                    description ={event.description}
+                    attendance={event.attendance_count}
+                   
+                    avatar={event.featured_photo}
+                    
+                />
+            )}
+            </ul>
+        </div>
         )
     }
-}
-
+}   
