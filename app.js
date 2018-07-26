65b5cf582e113af39957fdfc1fe358fcaa9ea8d3
loelias <loe_elias@msn.com>
2018-07-25 21:23:28 -0300

    Fix app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 3000
 
 app.get('/events', (req, res) => {
     res.send('Hello World!')
-} 
+}); 
 
 app.listen(5000, () => {console.log(`Example app listening on port 5000!`)})
