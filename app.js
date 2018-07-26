a9efc3a81b3a4a1f7f23c747bcec33026d93d368
loelias <loe_elias@msn.com>
2018-07-25 21:39:16 -0300

    Task03 done- proxy no backend que recebe parametros via request

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const request = require('request')
-const PORT = process.env.PORT || 3000
-
+const PORT = 5000
+const URL = 'https://api.meetup.com/find/upcoming_events'
 
 
 app.get('/events', (req, res) => {
-    const URL = 'https://api.meetup.com/find/upcoming_events'
-
     request.get({ url: URL, qs: req.query, json: true}, (err, _, body) => {
         err ? res.json(err): res.json(body)
     })
 }) 
-
-
-app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Example app listening on port ${ PORT }!`))
