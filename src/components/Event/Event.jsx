618d87aff4345e0fff27981fdf0f3b3c7fa0b095
Pedro Ferreira <peferrera@outlook.com>
2018-08-07 16:38:21 -0300

    #TASK 07 - Formatar a descrição do evento retornado pela Api. Closes #9

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
 import './Event.css';
 export default class Event extends React.Component {
   render() {
@@ -11,21 +12,21 @@ export default class Event extends React.Component {
           <span className="event__date">
             <p>{props.date} </p>
           </span>
-          <img className="event__avatar" alt={props.name} src={props.avatar ? props.avatar.photo_link : 'https://www.zndrke.com/dashboard/img/placeholder.png'} />
+          <img href={props.link} className="event__avatar" alt={props.name} src={props.avatar ? props.avatar.photo_link : 'https://www.zndrke.com/dashboard/img/placeholder.png'} />
         </div>
         <div className="event__info" >
           <span className="event__place">
             <p>{props.place} | {props.address} </p>
           </span>
           <span className="event__name">
-            <h2>{props.name}</h2>
+            <h2><a href={props.link} target="_blank">{props.name}</a></h2>
           </span>
           <span className="event__time">
             <p>{props.time}</p>
           </span>
         </div>
         <div className="event__description">
-          <p> {props.description}</p>
+          <p> {ReactHtmlParser(props.description)}</p>
         </div>
       </div>
     )
